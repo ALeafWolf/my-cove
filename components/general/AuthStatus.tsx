cc42b9d1e5ec3bd0680f6a4c0c5d0791061ac179
AQ <aleafwolf@outlook.com>
2026-02-23 18:51:13 -0500

    edit AuthStatus

--- components/general/AuthStatus.tsx
+++ components/general/AuthStatus.tsx
@@ -2,6 +2,9 @@
 
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import drive from "@/assets/images/drive.png";
+import rotate from "@/assets/images/rotate.png";
 
 export default function AuthStatus() {
   const { data: _session, status } = useSession();
@@ -10,6 +13,9 @@ export default function AuthStatus() {
     <>
       {status === "authenticated" ? (
         <div className="flex items-center flex-col gap-8">
+          <div className="flex justify-center">
+            <Image src={drive} alt="drive" width={300} height={300} fetchPriority="high" />
+          </div>
           <button
             className="border px-4 py-2 rounded-md text-lg"
             onClick={() => signOut()}
@@ -18,13 +24,18 @@ export default function AuthStatus() {
           </button>
         </div>
       ) : (
-        <div className="flex justify-center">
-          <Link
-            className="block text-lg border px-4 py-2 rounded-md"
-            href="/login"
-          >
-            Login
-          </Link>
+        <div className="flex flex-col gap-8">
+          <div className="flex justify-center">
+            <Image src={rotate} alt="rotate" width={300} height={300} fetchPriority="high" />
+          </div>
+          <div className="flex justify-center">
+            <Link
+              className="block text-lg border px-4 py-2 rounded-md"
+              href="/login"
+            >
+              Login
+            </Link>
+          </div>
         </div>
       )}
     </>
